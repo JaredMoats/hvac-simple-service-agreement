5cefc08264ae01aacbad8b3c81e871f5632984b0
Jared Moats <JaredMoats@outlook.com>
2019-06-08 15:45:04 -0400

    Updated for first deploy to heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 //Connect to database
